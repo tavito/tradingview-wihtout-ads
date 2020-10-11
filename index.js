86bcd31fd212b8215d012a630c51aeccb7d54189
tavito <gatala@gmail.com>
2020-10-11 20:08:40 -0300

    Added an observer to remove 'Go Pro' ad

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-// select the target node (ads container)
-var target = document.querySelector("[data-role='toast-container']");
+// configuration of the observers
+var config = { attributes: true, childList: true, characterData: true };
 
-// create an observer instance
+// select the ads node (ads container)
+const ads = document.querySelector("[data-role='toast-container']");
+// console.log("ads", ads);
+let goPro;
+// Hide "Go Pro" ad
+window.onload = function () {
+  goPro = document.getElementById("overlap-manager-root");
+  observerGoPro.observe(goPro, config);
+};
+
+// create an observer instance for ads
 var observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
     if (mutation.type == "childList") {
       if (mutation.addedNodes.length >= 1) {
         for (var i = 0; i < mutation.addedNodes.length; i++) {
-          console.log("Ad removed!");
           // remove ads when it appears
-          mutation.target.removeChild(mutation.addedNodes[i]);
+          ads.removeChild(mutation.addedNodes[i]);
+          console.log("Ad removed!");
         }
       }
     }
   });
 });
 
-// configuration of the observer:
-var config = { attributes: true, childList: true, characterData: true };
+// create an observer instance for "Go Pro" ad
+var observerGoPro = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.type == "childList") {
+      if (mutation.addedNodes.length >= 1) {
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+          if (
+            mutation.addedNodes[0].nextElementSibling // &&
+          ) {
+            mutation.addedNodes[0].nextElementSibling.childNodes[0].remove();
+            console.log("Go Pro ad removed!");
+          }
+        }
+      }
+    }
+  });
+});
 
-// pass in the target node, as well as the observer options
-observer.observe(target, config);
+// pass in the ads node, as well as the observer options
+observer.observe(ads, config);
